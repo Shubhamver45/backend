13e758675a8802a06c6adf60a1323e8d6ff9581c
shubham <shubhamvernerkar450@gmail.com>
2026-05-08 15:09:58 +0530

    fix(backend): force IPv4 dns resolution to prevent nodemailer ENETUNREACH ipv6 crash on render

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // backend/server.js
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const cors = require('cors');
 
